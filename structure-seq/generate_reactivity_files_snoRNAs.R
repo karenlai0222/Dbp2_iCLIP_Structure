a588be50a604d696e799375ef35974edc6cf6a2c
karenlai0222 <lai64@purdue.edu>
2019-03-27 16:08:38 -0400

    Update and rename generate_reactivity_files.R to generate_reactivity_files_snoRNAs.R

--- structure-seq/generate_reactivity_files_snoRNAs.R
+++ structure-seq/generate_reactivity_files_snoRNAs.R
@@ -1,10 +1,10 @@
-load("Reactivity.RData")
+load("snoRNA_reactivity.RData")
 
 filenames <- c("wt1.reac", "wt2.reac", "wt3.reac", "mt1.reac", "mt3.reac")
 
 for (r in 1:5) {
   sink(filenames[r])
-  for (i in 1:length(reac_norm)) {
+  for (i in 1:length(reac_norm_snoRNA)) {
     curr_rna = names(counts)[i]
     curr_cov = coverages_all[curr_rna]
     curr_reac = reac_norm[[curr_rna]][, strsplit(filenames[r], split= ".reac")[[1]][1]]
